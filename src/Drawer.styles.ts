b94c96273cd09b527045a15f902eb80ffa8e10d0
Ilker Kilic <ilkrklc@gmail.com>
2022-02-06 03:35:22 +0300

    Separate drawer styles

---
+++ src/Drawer.styles.ts
@@ -0,0 +1,26 @@
+import { CSSProperties } from 'react';
+
+import { DrawerDefaults } from './Drawer.defaults';
+
+export const wrapperStyles: CSSProperties = {
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  bottom: 0,
+  right: 0,
+  zIndex: DrawerDefaults.DRAWER_Z_INDEX,
+  backgroundColor: 'transparent',
+};
+
+export const baseContainerStyles: CSSProperties = {
+  position: 'fixed',
+  boxSizing: 'border-box',
+  backgroundColor: '#ffffff',
+  overflowY: 'auto',
+  height: '100%',
+  width: '100%',
+  boxShadow: '0px 10px 40px 0px rgb(0 0 0 / 80%)',
+  zIndex: DrawerDefaults.DRAWER_Z_INDEX + 1,
+  paddingTop: '1rem',
+  paddingBottom: '1rem',
+};
